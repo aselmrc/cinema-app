eb0040eba07f6a37d0a36f26e0b192264494a34f
Asel <aselft08@gmail.com>
2020-09-26 13:27:35 +0600

    added slider component

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainContent from '../content/main-content/MainContent';
+import './Main.scss';
+
+function Main(props) {
+  return (
+    <div className="main">
+      <MainContent />
+    </div>
+  );
+}
+
+export default Main;
