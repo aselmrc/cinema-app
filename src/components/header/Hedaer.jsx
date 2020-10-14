a9e70cdd9ce42455cab7c6607962ec4323d6b04f
Asel <aselft08@gmail.com>
2020-10-14 14:55:20 +0600

    added api-service

--- src/components/header/Hedaer.jsx
+++ src/components/header/Hedaer.jsx
@@ -1,22 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getMovies, setMovieType, setResponsePageNumber } from '../../redux/actions/movie';
+import PropTypes from 'prop-types';
+import { useHistory, useLocation, useRouteMatch } from 'react-router-dom';
 import logo from '../../logo.png';
 import './Header.scss';
+import { setError } from '../../redux/actions/errors';
+import { pathURL } from '../../redux/actions/routes';
 
-function Header(props) {
+function Header({ setMovieType, getMovies, page, totalPages, setResponsePageNumber, routesArray, path, url, pathURL, setError, errors }) {
   let [navClass, setNavClass] = useState(false);
   let [menuClass, setMenuClass] = useState(false);
+  const [type, setType] = useState('now-playing');
+  const [hideHeader, setHideHeader] = useState(false);
+  const [disableSearch, setDisableSearch] = useState(false);
 
-  const toggleMenu = () => {
-    menuClass = !menuClass;
-    navClass = !navClass;
-    setNavClass(navClass);
-    setMenuClass(menuClass);
-    if (navClass) {
-      document.body.classList.add('header-nav-open');
-    } else {
-      document.body.classList.remove('header-nav-open');
-    }
-  };
+  const history = useHistory();
+  const location = useLocation();
+  const detailsRoute = useRouteMatch('/:id/:name/details');
 
   const HEADER_LIST = [
     {
@@ -45,36 +46,119 @@ function Header(props) {
     }
   ];
 
+  useEffect(() => {
+    if (routesArray.length) {
+      if (!path && !url) {
+        pathURL('/', '/');
+        const error = new Error(`Page with pathname ${location.pathname} not found with status code 404.`);
+        setError({ message: `Page with pathname ${location.pathname} not found.`, statusCode: 404 });
+        throw error;
+      }
+    }
+  }, [path, url, routesArray, pathURL]);
+
+  useEffect(() => {
+    if (errors.message || errors.statusCode) {
+      pathURL('/', '/');
+      const error = new Error(`${errors.message} With status code ${errors.statusCode} `);
+      setError({ message: `Page with pathname ${location.pathname} not found.`, statusCode: 404 });
+      throw error;
+    }
+  }, [errors]);
+
+  useEffect(() => {
+    if (path && !errors.message && !errors.statusCode) {
+      getMovies(type, page);
+      setResponsePageNumber(page, totalPages);
+      if (detailsRoute || location.pathname === '/') {
+        setHideHeader(true);
+      }
+
+      if (location.pathname !== '/' && location.key) {
+        setDisableSearch(true);
+      }
+    }
+  }, [type, disableSearch, location, path]);
+
+  const setMovieTypeUrl = (type) => {
+    setDisableSearch(false);
+    if (location.pathname !== '/') {
+      history.push('/');
+      setType(type);
+      setMovieType(type);
+    } else {
+      setType(type);
+      setMovieType(type);
+    }
+  };
+
+  const toggleMenu = () => {
+    menuClass = !menuClass;
+    navClass = !navClass;
+    setNavClass(navClass);
+    setMenuClass(menuClass);
+    if (navClass) {
+      document.body.classList.add('header-nav-open');
+    } else {
+      document.body.classList.remove('header-nav-open');
+    }
+  };
+
   return (
     <>
-      <div className="header-nav-wrapper">
-        <div className="header-bar">
-          <div className="header-navbar">
-            <div className="header-image">
-              <img src={logo} alt="logo" />
+      {hideHeader && (
+        <div className="header-nav-wrapper">
+          <div className="header-bar">
+            <div className="header-navbar">
+              <div className="header-image">
+                <img src={logo} alt="logo" />
+              </div>
+              <div className={`${menuClass ? 'header-menu-toggle is-active' : 'header-menu-toggle'}`} id="header-mobile-menu" onClick={() => toggleMenu()}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+              </div>
+              <ul className={`${navClass ? 'header-nav header-mobile-nav' : 'header-nav'}`}>
+                {HEADER_LIST.map((data) => (
+                  <li key={data.id} className={data.type === type ? 'header-nav-item active-item' : 'header-nav-item'} onClick={() => setMovieTypeUrl(data.type)}>
+                    <span className="header-list-name">
+                      <i className={data.iconClass}></i>
+                    </span>
+                    &nbsp;
+                    <span className="header-list-name">{data.name}</span>
+                  </li>
+                ))}
+                <input className={`search-input ${disableSearch ? 'disabled' : ''}`} type="text" placeholder="Search for a movie" />
+              </ul>
             </div>
-            <div className={`${menuClass ? 'header-menu-toggle is-active' : 'header-menu-toggle'}`} id="header-mobile-menu" onClick={() => toggleMenu()}>
-              <span className="bar"></span>
-              <span className="bar"></span>
-              <span className="bar"></span>
-            </div>
-            <ul className={`${navClass ? 'header-nav header-mobile-nav' : 'header-nav'}`}>
-              {HEADER_LIST.map((data) => (
-                <li key={data.id} className="header-nav-item">
-                  <span className="header-list-name">
-                    <i className={data.iconClass}></i>
-                  </span>
-                  &nbsp;
-                  <span className="header-list-name">{data.name}</span>
-                </li>
-              ))}
-              <input type="text" className="search-input" placeholder="Search for a movie" />
-            </ul>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
 
-export default Header;
+Header.propTypes = {
+  getMovies: PropTypes.func,
+  setMovieType: PropTypes.func,
+  path: PropTypes.string,
+  page: PropTypes.number,
+  setResponsePageNumber: PropTypes.func,
+  totalPages: PropTypes.number,
+  url: PropTypes.string,
+  routesArray: PropTypes.array,
+  pathURL: PropTypes.func,
+  setError: PropTypes.func,
+  errors: PropTypes.object
+};
+
+const mapStateToProps = (state) => ({
+  page: state.movies.page,
+  totalPages: state.movies.totalPages,
+  routesArray: state.routes.routesArray,
+  path: state.routes.path,
+  url: state.routes.url,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps, { getMovies, setMovieType, setResponsePageNumber, setError, pathURL })(Header);
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,13 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import MainContent from '../content/main-content/MainContent';
+import { loadMoreMovies, setResponsePageNumber } from '../../redux/actions/movie';
+import Spinner from '../spinner/Spinner';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import './Main.scss';
+import { pathURL } from '../../redux/actions/routes';
 
 function Main(props) {
+  const { loadMoreMovies, page, errors, totalPages, setResponsePageNumber, pathURL, match } = props;
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(page);
+  const mainRef = useRef();
+  const bottomLineRef = useRef();
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  useEffect(() => {
+    pathURL(match.path, match.url);
+    setResponsePageNumber(currentPage, totalPages);
+    loadMoreMovies('now_playing', currentPage);
+    // eslint-disable-next-line
+  }, [currentPage, totalPages]);
+
+  const fetchData = () => {
+    let pageNumber = currentPage;
+    if (page < totalPages) {
+      pageNumber += 1;
+      setCurrentPage(pageNumber);
+    }
+  };
+
+  const handleScroll = () => {
+    const containerHeight = mainRef.current.getBoundingClientRect().height;
+    const { top: bottomLineTop } = bottomLineRef.current.getBoundingClientRect();
+    if (bottomLineTop <= containerHeight) {
+      fetchData();
+    }
+  };
   return (
-    <div className="main">
-      <MainContent />
-    </div>
+    <>
+      {!errors.message && !errors.statusCode && (
+        <div className="main" ref={mainRef} onScroll={() => handleScroll()}>
+          {loading ? <Spinner /> : <MainContent />}
+          <div ref={bottomLineRef}></div>
+        </div>
+      )}
+    </>
   );
 }
 
-export default Main;
+Main.propTypes = {
+  list: PropTypes.array,
+  page: PropTypes.number,
+  totalPages: PropTypes.number,
+  loadMoreMovies: PropTypes.func,
+  setResponsePageNumber: PropTypes.func,
+  match: PropTypes.object,
+  pathURL: PropTypes.func,
+  errors: PropTypes.object
+};
+
+const mapStateToProps = (state) => ({
+  list: state.movies.list,
+  page: state.movies.page,
+  totalPages: state.movies.totalPages,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps, { loadMoreMovies, setResponsePageNumber, pathURL })(Main);
