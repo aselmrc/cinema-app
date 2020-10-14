d4718a5d37cff71a24de2a429c5d5ce6c16d12dd
Asel <aselft08@gmail.com>
2020-10-14 18:53:56 +0600

    added search component

---
+++ src/components/content/search-result/SearchResult.jsx
@@ -0,0 +1,66 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { v4 as uuidv4 } from 'uuid';
+import LazyImage from '../../lazy-image/LazyImage';
+import { Link } from 'react-router-dom';
+import { IMAGE_URL } from '../../../services/movie.services';
+import Rating from '../rating/Rating';
+import '../grid/Grid.scss';
+import './SearchResult.scss';
+
+function SearchResult({ searchResult, searchQuery }) {
+  const [movieData, setMovieData] = useState([]);
+
+  useEffect(() => {
+    setMovieData(searchResult);
+  }, [searchResult]);
+
+  const formatMovieTitle = (title) => {
+    const titleStr = title.toLowerCase();
+    return titleStr.replace(/ /g, '-');
+  };
+
+  return (
+    <div className="searchKeyword">
+      <div className="grid-search-title">
+        <span className="grid-text1">Your search keyword:</span> <span className="grid-text2">{searchQuery}</span>
+      </div>
+      <div className="grid">
+        {movieData.map((data) => (
+          <Fragment key={uuidv4()}>
+            {data.poster_path && (
+              <LazyImage className="grid-cell" src={`${IMAGE_URL}${data.poster_path}`}>
+                <div className="grid-read-more">
+                  <button className="grid-cell-button">
+                    <Link to={`/${data.id}/${formatMovieTitle(data.title)}/details`}>Read More</Link>
+                  </button>
+                </div>
+                <div className="grid-detail">
+                  <span className="grid-detail-title">{data.title}</span>
+                  <div className="grid-detail-rating">
+                    <Rating rating={data.vote_average} totalStars={10} />
+                    &nbsp;&nbsp;
+                    <div className="grid-vote-average">{data.vote_average}</div>
+                  </div>
+                </div>
+              </LazyImage>
+            )}
+          </Fragment>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+SearchResult.propTypes = {
+  searchResult: PropTypes.array.isRequired,
+  searchQuery: PropTypes.string.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  searchResult: state.movies.searchResult,
+  searchQuery: state.movies.searchQuery
+});
+
+export default connect(mapStateToProps, {})(SearchResult);
